2398ea843517e45855fa6184d87b1306f98fb928
xiaolait <xiaolait@outlook.com>
2020-06-12 19:01:18 +0800

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PipelineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
